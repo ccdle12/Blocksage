021c526605fa37ea48a7541111751069727ada63
ccdle12 <chris.coverdale24@gmail.com>
2018-06-08 23:08:25 +0100

    Refactoring to use base64-auth

--- api/tests/base64-auth.test.js
+++ api/tests/base64-auth.test.js
@@ -1,8 +1,8 @@
-const base64Auth = require('../utils/base64-auth')
+const base64_auth = require('../utils/base64-auth')
 
 describe('base64Auth', () => {
     it('should encode environment variables username:password to base64', () => {
-        const result = base64Auth.getEncodedAuth()
+        const result = base64_auth.getEncodedAuth()
         expect(result).toEqual('Yml0Y29pbjpwYXNzd29yZA==')
     })
 })
--- api/utils/bitcoin-requests.js
+++ api/utils/bitcoin-requests.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const config = require('config')
 
 const request = require('request')
-const base64Auth = require('../utils/base64-auth')
+const base64_auth = require('./utils/base64-auth')
 
 module.exports.send = (method_request, callback) => { 
     const options = {
         url: `http://${config.get('domain')}:${config.get('node-rpc')}`,
         headers: {
-            Authorization: `Basic ${base64Auth.getEncodedAuth()}`
+            Authorization: `Basic ${base64_auth.getEncodedAuth()}`
         },
         body: JSON.stringify({
             method: method_request
