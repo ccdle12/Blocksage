bba597780b706ba58b4dee6e5465a499c9924868
ccdle12 <chris.coverdale24@gmail.com>
2018-06-08 15:45:05 +0100

    Adding morgan logging tool in development-docker

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const config = require('config')
 const debug = require('debug')('app:startup')
+const morgan = require('morgan')
 
 const express = require('express')
 const networkInfo = require('./routes/network-info')
 
 const app = express()
+
+if (app.get('env') === 'development-docker') {
+    app.use(morgan('tiny'))
+    debug("Morgan Enabled...")
+}
 
 app.use(express.json())
 app.use('/api/network-info', networkInfo)
