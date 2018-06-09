cc4946b511b04321ecdf0ff847883752b6b1d063
ccdle12 <chris.coverdale24@gmail.com>
2018-06-09 11:30:22 +0100

    Refactoring styling to have braces on new line

--- api/utils/base64-auth.js
+++ api/utils/base64-auth.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const config = require('config')
 
-module.exports.getEncodedAuth = () => {
+module.exports.getEncodedAuth = () =>
+{
     return Buffer.from(`${config.get('username')}:${config.get('password')}`).toString('base64')
 }
