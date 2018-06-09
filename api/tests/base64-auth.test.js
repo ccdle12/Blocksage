05c3e0b4aa92f0b7796887542f8494a5ca1404e0
ccdle12 <chris.coverdale24@gmail.com>
2018-06-09 11:25:27 +0100

    Removing whitespaces from files

--- api/tests/base64-auth.test.js
+++ api/tests/base64-auth.test.js
@@ -1,7 +1,9 @@
 const base64_auth = require('../utils/base64-auth')
 
-describe('base64Auth', () => {
-    it('should encode environment variables username:password to base64', () => {
+describe('base64Auth', () =>
+{
+    it('should encode environment variables username:password to base64', () =>
+    {
         const result = base64_auth.getEncodedAuth()
         expect(result).toEqual('Yml0Y29pbjpwYXNzd29yZA==')
     })
