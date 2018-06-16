3f8494e07661142ce6f89989261ae297dfb458fa
ccdle12 <chris.coverdale24@gmail.com>
2018-06-16 18:47:20 +0100

    Adding debug prints to bitcoin-requests

--- api/bitcoin-node/bitcoin-requests.js
+++ api/bitcoin-node/bitcoin-requests.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const config = require('config')
+const debug = require('debug')('app:bitcoin-requests')
 
 const request = require('request')
 const base64_auth = require('../utils/base64-auth')
 
 module.exports.send = (method_request, callback) => 
 { 
+    debug(`http://${config.get('domain')}:${config.get('node-rpc')}`)
+    debug(`Authorization Basic ${base64_auth.getEncodedAuth()}`)
+
     const options =
     {
         url: `http://${config.get('domain')}:${config.get('node-rpc')}`,
