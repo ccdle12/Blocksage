8f54cc5968a9264ba71164539b96550ee75015a8
ccdle12 <chris.coverdale24@gmail.com>
2018-06-17 10:49:27 +0100

    Parsing body as passed as string and moving statements into switch

--- api/bitcoin-node/validate-response.js
+++ api/bitcoin-node/validate-response.js
@@ -1,24 +1,26 @@
 const debug = require('debug')('app:validate-response')
 
-module.exports = (err, body) => 
+module.exports = (body) =>
 {
-    if (err) 
-    {
-        debug(`Error: ${err}`)
-        return { status_code: 502, message: 'Bad Gateway - Node unresponsive' }
-    }
-    
     const json_body = JSON.parse(body)
-    debug(`Body: ${JSON.stringify(json_body)}`)
 
-    if (json_body.error && json_body.error.code === -32601)
-        return { status_code: 404, message: 'Method request not found' }
+    if (json_body.error)
+    {
+        switch(json_body.error.code)
+        {
+            case 502:
+                return { status_code: 502, message: "Bad Gateway - Node unresponsive" }
 
-    if (json_body.error && json_body.error.code === -3)
-        return { status_code: 404, message: 'Bad input type - not found, does not exist or incorrect input format'}
+            case -32601:
+                return { status_code: 404, message: 'Method request not found' }
 
-    if (json_body.error && json_body.error.code === -5)
-        return { status_code: 404, message: 'Block not found'}
+            case -3:
+                return { status_code: 404, message: 'Bad input type - not found, does not exist or incorrect input format'}
+
+            case -5:
+                return { status_code: 404, message: 'Block not found'}
+        }
+    }
 
     return { status_code: 200, message: 'OK' }
 }
