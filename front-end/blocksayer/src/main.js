fe839ae6fae65096f1fe158fd91628f12a9d2b79
ccdle12 <chris.coverdale24@gmail.com>
2018-06-18 11:31:07 +0100

    Adding VueResource dependency to main.js

--- front-end/blocksayer/src/main.js
+++ front-end/blocksayer/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VeeValidate from 'vee-validate'
+import VueResource from 'vue-resource'
 import { routes } from './routes'
 
 Vue.config.productionTip = false
 Vue.use(VeeValidate)
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const router = new VueRouter({
   routes,
