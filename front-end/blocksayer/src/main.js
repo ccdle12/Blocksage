955a2e94706cebc84be2abcde78971227306b4f1
ccdle12 <chris.coverdale24@gmail.com>
2018-06-20 23:11:59 +0100

    Replacing front-end with webpack build

--- front-end/blocksayer/src/main.js
+++ front-end/blocksayer/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import VeeValidate from 'vee-validate'
 import VueResource from 'vue-resource'
 import { routes } from './routes'
 
 Vue.config.productionTip = false
-Vue.use(VeeValidate)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
