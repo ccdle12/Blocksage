22481ff7475d3d499df9f3932dd98ccc2e1e02f7
ccdle12 <chris.coverdale24@gmail.com>
2018-06-21 22:24:03 +0100

    Updating import statements and depedencies

--- front-end/blocksage/src/main.js
+++ front-end/blocksage/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { routes } from './routes'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = process.env.NODE_ENV === 'development'
+console.log('Main js: ', process.env.API_BASE_URL)
 Vue.use(VueRouter)
-Vue.use(VueResource)
+Vue.use(VueAxios, axios)
 
 const router = new VueRouter({
   routes,
