b6e8afc79b55dc4e6756caf7456b796afb570c46
ccdle12 <chris.coverdale24@gmail.com>
2018-06-21 22:25:34 +0100

    Adding TODO item in Tx.vue

--- front-end/blocksage/src/routes.js
+++ front-end/blocksage/src/routes.js
@@ -3,5 +3,6 @@ import Tx from './components/tx/Tx.vue'
 
 export const routes = [
     { path: '', component: Home },
+    //TODO: Create route guard to make sure there is a txHash parameter
     { path: '/tx/:txHash', component: Tx, name: 'tx' }
 ]
