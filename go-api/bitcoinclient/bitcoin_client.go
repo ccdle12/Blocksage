a76d24a9edf5f401bc65792f489319195c36c454
ccdle12 <chris.coverdale24@gmail.com>
2018-07-19 22:00:24 +0100

    Renaming bitcoin client go files

---
+++ go-api/bitcoinclient/bitcoin_client.go
@@ -0,0 +1,59 @@
+package bitcoinclient
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// BitcoinClient is the struct that builds the requests, sends the requests and validates
+// responses from the Bitcoin Node RPC
+type BitcoinClient struct {
+	Client          *http.Client
+	BitcoinNodeAddr string
+	bitcoinRPC      bitcoinRPC
+}
+
+func init() {
+	// Log Print the line number of where the error occurred
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Write error log prints to log.txt
+	nf, err := os.Create("./bitcoinclient/log.txt")
+	if err != nil {
+		fmt.Println("Non-Crucial Internal Error - Cannot create log.txt file for bitcoinclient.go")
+		return
+	}
+
+	log.SetOutput(nf)
+}
+
+// GetNetworkInfo returns a string response from the Bitcoin Node requesting "getnetworkinfo"
+// "getnetworkinfo" returns information about the node's conection to the network
+func (b *BitcoinClient) GetNetworkInfo() (string, error) {
+	fmt.Println(b.BitcoinNodeAddr)
+	req, err := b.createRequest("getnetworkinfo")
+	if err != nil {
+		return err.Error(), err
+	}
+
+	res, err := b.sendRequest(req)
+	if err != nil {
+		return err.Error(), err
+	}
+	defer res.Body.Close()
+
+	resStr, err := b.convBodyToStr(res)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	// Bitcoin Node returns an empty string if authentication fails and also returns a JSON for RPC errors
+	rpcError := b.AuthenticateRPCResponse(resStr)
+	if rpcError != nil {
+		return rpcError.Error(), rpcError
+	}
+
+	return resStr, nil
+}
---
+++ go-api/bitcoinclient/bitcoin_rpc.go
@@ -0,0 +1,82 @@
+package bitcoinclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+// BitcoinRPC describes an expected set of behaviours to create/send requests and create a body
+type bitcoinRPC interface {
+	createRequest(s string) (*http.Request, error)
+	createBody(s string) (*bytes.Buffer, error)
+	sendRequest(r *http.Request) (*http.Response, error)
+	convBodyToStr(r *http.Response) (string, error)
+}
+
+// methodBody is a struct for the body when sending POST requests to the Bitcoin Node RPC
+type methodBody struct {
+	Method string `json:"method"`
+}
+
+var (
+	username = os.Getenv("USERNAME")
+	password = os.Getenv("PASSWORD")
+)
+
+// createRequest builds a request to send the Bitcoin Node
+func (b *BitcoinClient) createRequest(methodType string) (*http.Request, error) {
+	body, err := b.createBody(methodType)
+	if err != nil {
+		log.Println(ErrCreatingBody)
+		return nil, ErrCreatingBody
+	}
+
+	req, err := http.NewRequest("POST", b.BitcoinNodeAddr, body)
+	if err != nil {
+		log.Println(ErrCreatingRequest)
+		return nil, ErrCreatingRequest
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(username, password)
+
+	return req, nil
+}
+
+// createBody, creates a body specifying the RPC methods to request from the Bitcoin Node
+func (b *BitcoinClient) createBody(methodType string) (*bytes.Buffer, error) {
+	body := methodBody{methodType}
+	bodyJSON, err := json.Marshal(body)
+	if err != nil {
+		log.Println(ErrCreatingBody)
+		return nil, ErrCreatingBody
+	}
+
+	return bytes.NewBuffer(bodyJSON), nil
+}
+
+// sendRequest, sends the http request to the Bitcoin Node
+func (b *BitcoinClient) sendRequest(req *http.Request) (*http.Response, error) {
+	res, err := b.Client.Do(req)
+	if err != nil {
+		log.Println(ErrUnresponsive)
+		return nil, ErrUnresponsive
+	}
+
+	return res, nil
+}
+
+// convBodyToStr, converts the whole body of a response to string
+func (b *BitcoinClient) convBodyToStr(res *http.Response) (string, error) {
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+		return "", ErrConvertingBodyToString
+	}
+
+	return string(bodyBytes), nil
+}
