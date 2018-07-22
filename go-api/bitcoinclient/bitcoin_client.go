945b2fcf903f33281b6c01ddf60ad6561bafd7cf
ccdle12 <chris.coverdale24@gmail.com>
2018-07-22 01:04:37 +0100

    Adding functions for decode raw tx and refactoring

--- go-api/bitcoinclient/bitcoin_client.go
+++ go-api/bitcoinclient/bitcoin_client.go
@@ -1,14 +1,14 @@
 package bitcoinclient
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-// BitcoinClient is the struct that builds the requests, sends the requests and validates
-// responses from the Bitcoin Node RPC
+// BitcoinClient is the struct responsible for communicating to the Bitcoin Node
 type BitcoinClient struct {
 	Client          *http.Client
 	BitcoinNodeAddr string
@@ -28,11 +28,37 @@ func init() {
 	log.SetOutput(nf)
 }
 
-// GetNetworkInfo returns a string response from the Bitcoin Node requesting "getnetworkinfo"
-// "getnetworkinfo" returns information about the node's conection to the network
+// GetNetworkInfo returns a string response from the Bitcoin Node, requesting information about the node's conection to the network
 func (b *BitcoinClient) GetNetworkInfo() (string, error) {
-	fmt.Println(b.BitcoinNodeAddr)
-	req, err := b.createRequest("getnetworkinfo")
+	return b.getResponse(&RPCBody{"getnetworkinfo", []string{}})
+}
+
+// GetBlock returns a string response from the Bitcoin Node, requesting information of a Block according the blockHash passed as an argument
+func (b *BitcoinClient) GetBlock(blockHash string) (string, error) {
+	return b.getResponse(&RPCBody{"getblock", []string{blockHash}})
+}
+
+// GetTransaction returns a string response from the Bitcoin Node, requesting information on a Transaction according to the TransactionHash passed as an argument
+func (b *BitcoinClient) GetTransaction(txHash string) (string, error) {
+	response, err := b.getResponse(&RPCBody{"getrawtransaction", []string{txHash}})
+	if err != nil {
+		return err.Error(), err
+	}
+
+	return b.decodeRawTransaction(response)
+}
+
+// decodeRawTransaction returns a string response from the Bitcoin Node, it takes in a rawTx string and decodes it to human readable format
+func (b *BitcoinClient) decodeRawTransaction(rawTx string) (string, error) {
+	var rpcResponse RPCBitcoinResponse
+	json.Unmarshal([]byte(rawTx), &rpcResponse)
+
+	return b.getResponse(&RPCBody{"decoderawtransaction", []string{*rpcResponse.Result}})
+}
+
+// getResponse is the generic function that will take in a *RPCBody struct and get a reponse from the Bitcoin Node
+func (b *BitcoinClient) getResponse(rpcBody *RPCBody) (string, error) {
+	req, err := b.createRequest(rpcBody)
 	if err != nil {
 		return err.Error(), err
 	}
@@ -48,10 +74,10 @@ func (b *BitcoinClient) GetNetworkInfo() (string, error) {
 		return err.Error(), err
 	}
 
-	// Bitcoin Node returns an empty string if authentication fails and also returns a JSON for RPC errors
+	// Bitcoin Node returns an empty string if authentication fails and also returns a JSON for RPC requests
 	rpcError := b.AuthenticateRPCResponse(resStr)
 	if rpcError != nil {
-		return rpcError.Error(), rpcError
+		return err.Error(), err
 	}
 
 	return resStr, nil
