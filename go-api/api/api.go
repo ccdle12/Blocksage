65819ab568acecd28efbebe783862abf4ba7d210
ccdle12 <chris.coverdale24@gmail.com>
2018-07-22 01:03:20 +0100

    Adding Blocks and Transactions routes

--- go-api/api/api.go
+++ go-api/api/api.go
@@ -7,15 +7,40 @@ import (
 	"net/http"
 )
 
-// API struct is responsible for connecting the end user with the below functions for each route specified in main
+// API struct is responsible for connecting the requests from the client side with functions specific to each route
 type API struct {
 	BitcoinClient *bitcoinclient.BitcoinClient
-	Router        *mux.Router
 }
 
 // NetworkInfo is responsible for returning information about the Bitcoin Nodes connection to the network
 func (a *API) NetworkInfo(w http.ResponseWriter, r *http.Request) {
 	response, err := a.BitcoinClient.GetNetworkInfo()
+	if err != nil {
+		handledWriter := a.BitcoinClient.HandleStatusCodeError(w, err)
+		fmt.Fprintf(handledWriter, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, response)
+}
+
+// Blocks is responsible for returning information about a certain Block in the Bitcoin Blockchain
+func (a *API) Blocks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	response, err := a.BitcoinClient.GetBlock(vars["blockhash"])
+	if err != nil {
+		handledWriter := a.BitcoinClient.HandleStatusCodeError(w, err)
+		fmt.Fprintf(handledWriter, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, response)
+}
+
+// Transactions is responsible for returning information about a certain Transaction in the Bitcoin Blockchain
+func (a *API) Transactions(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	response, err := a.BitcoinClient.GetTransaction(vars["txhash"])
 	if err != nil {
 		handledWriter := a.BitcoinClient.HandleStatusCodeError(w, err)
 		fmt.Fprintf(handledWriter, err.Error())
