ea07d22bd9b2164c96bd6890f4f9d7b2747e093f
ccdle12 <chris.coverdale24@gmail.com>
2018-07-28 07:34:01 +0100

    Adding braces around if statement in navigation service

--- front-end/blocksage/src/components/navigation/NavigationService.js
+++ front-end/blocksage/src/components/navigation/NavigationService.js
@@ -1,7 +1,8 @@
 export default {
     navigate: function(router, searchRequest, searchParams) {
-        if (searchRequest.valid && searchRequest.type == 'tx') 
+        if (searchRequest.valid && searchRequest.type == 'tx') {
             router.push({ name: searchRequest.type, params: { txHash: searchParams } })
+        }
 
         return false
     },
