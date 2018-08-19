d3a15e861a7d5e2f85229088c96d5611bcac90cd
ccdle12 <chris.coverdale24@gmail.com>
2018-08-19 20:40:41 +0100

    go-crawler/main: removing unused features

--- go-crawler/main.go
+++ go-crawler/main.go
@@ -10,33 +10,25 @@ import (
 	"github.com/ccdle12/Blocksage/go-crawler/bitcoinclient"
 	"github.com/ccdle12/Blocksage/go-crawler/bitcoincrawler"
 	"github.com/ccdle12/Blocksage/go-crawler/dbhandler"
-	"github.com/ccdle12/Blocksage/go-crawler/tables"
 )
 
+// The following variables are retreived environment variables
 var (
 	btcMainDomain = os.Getenv("BTC_MAIN_DOMAIN")
 )
 
 func main() {
+	// Create the connection to the DB
 	dbHandler, err := dbhandler.New("172.25.0.4", "5432", "postgres-dev", "12345", "dev", "postgres")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	// TODO (ccdle12): Closing db when debugging
-	// dbHandler.Close()
-
-	blockTable, err := blocktable.New(dbHandler)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	blockTable.CreateTable()
 
 	bitcoinClient := &bitcoinclient.BitcoinClient{
 		Client:          &http.Client{Timeout: time.Duration(5 * time.Second)},
 		BitcoinNodeAddr: fmt.Sprintf("http://%s:8332", btcMainDomain),
 	}
 
-	bitcoinCrawler := bitcoincrawler.New(bitcoinClient, dbHandler, blockTable)
+	bitcoinCrawler := bitcoincrawler.New(bitcoinClient, dbHandler)
 	bitcoinCrawler.Start()
 }
