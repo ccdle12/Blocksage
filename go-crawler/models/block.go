55f893d6d5f9ee23a8b90ea7bf71573400c9756e
ccdle12 <chris.coverdale24@gmail.com>
2018-09-07 09:20:14 +0100

    models: updating block model by deleting confirmations

--- go-crawler/models/block.go
+++ go-crawler/models/block.go
@@ -3,9 +3,9 @@ package models
 // Block is the struct which holds all the details of a Block from
 // the Bitcoin Blockchain.
 type Block struct {
-	ID                int64    `json:"-"`
+	// ID                int64    `json:"-"`
+	// Confirmations     int      `json:"confirmations"`
 	Hash              string   `json:"hash"`
-	Confirmations     int      `json:"confirmations"`
 	Strippedsize      int      `json:"strippedsize"`
 	Size              int      `json:"size"`
 	Weight            int      `json:"weight"`
