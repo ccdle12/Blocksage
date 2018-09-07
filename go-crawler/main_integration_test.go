72488b2fae0820bddee17c6c7b7f2d8e8ab7b6fa
ccdle12 <chris.coverdale24@gmail.com>
2018-09-07 09:19:56 +0100

    main: refactoring integration tests to use suite

--- go-crawler/main_integration_test.go
+++ go-crawler/main_integration_test.go
@@ -5,39 +5,51 @@ package main
 import (
 	"github.com/ccdle12/Blocksage/go-crawler/controllers"
 	"github.com/ccdle12/Blocksage/go-crawler/test-utils"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
 // NOTE: Ping() in db usecases does NOT respect context timeouts, testing dbClient.Connect()
 // will take longer than the other tests.
+// ===========================================================
+// Testing Suite
+// ===========================================================
+type MainIntegrationSuite struct {
+	suite.Suite
+}
 
+// This gets run automatically by `go test` so we call `suite.Run` inside it
+func TestSuiteMainIntegration(t *testing.T) {
+	// This is what actually runs our suite
+	suite.Run(t, new(MainIntegrationSuite))
+}
+
+// ===========================================================
+// Unit Tests
+// ===========================================================
 // TestDBConnection will create a DBClientUsecase and test we can open a connection to the DB.
-func TestDBConnection(t *testing.T) {
-	assert := assert.New(t)
-	// TODO (ccdle12): Look into VIPER and inject environment variables into utils
+func (suite *MainIntegrationSuite) TestDBConnection() {
 	dbClient, err := controllers.NewDBClient(testutils.DBHost, testutils.DBPort, testutils.DBUser,
 		testutils.DBPassword, testutils.DBName, testutils.DBType)
 
-	assert.NoError(err, "Should not return errors when initializing dbClient")
+	suite.NoError(err, "Should not return errors when initializing dbClient")
 
 	err = dbClient.Connect()
-	assert.NoError(err, "Should not return error when connecting to the DB")
-
 	defer dbClient.Close()
 
-	assert.NotNil(dbClient, "dbClient should not be nil")
+	suite.NoError(err, "Should not return error when connecting to the DB")
+	suite.NotNil(dbClient, "dbClient should not be nil")
 }
 
 // TestDBConnectionShouldFail will create a DBClientUsecase and test we can open a connection to the DB.
-func TestDBConnectionShouldFail(t *testing.T) {
-	assert := assert.New(t)
-
+func (suite *MainIntegrationSuite) TestDBConnectionShouldFail() {
 	dbClient, err := controllers.NewDBClient(testutils.IncorrectDBHost, testutils.DBPort, testutils.DBUser,
 		testutils.DBPassword, testutils.DBName, testutils.DBType)
 
-	assert.NoError(err, "Should not return errors when initializing dbClient")
+	suite.NoError(err, "Should not return errors when initializing dbClient")
 
 	err = dbClient.Connect()
-	assert.Error(err, "Should return error trying to connect to a DB that doesn't exist")
+	defer dbClient.Close()
+
+	suite.Error(err, "Should return error trying to connect to a DB that doesn't exist")
 }
