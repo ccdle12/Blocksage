d7323cbc2f8249a8336126df1279a9e729f1b1cf
ccdle12 <chris.coverdale24@gmail.com>
2018-09-08 14:23:56 +0100

    crawler/utils: updating errors to functions, returns simple and complex messages in the same error

--- go-crawler/utils/errors.go
+++ go-crawler/utils/errors.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 )
 
 // These Errors will be injected throughout the app
@@ -11,5 +12,21 @@ var (
 	ErrFailedToOpenDBConnection  = errors.New("Could not open connection to the DB")
 	ErrFailedToPingDB            = errors.New("Could not Ping DB connection")
 	ErrFailedToCloseDBConnection = errors.New("Failed to close DB connection")
-	ErrFailedToInsertToDB        = errors.New("Failed to insert row to the DB")
+	ErrFailedToInsertToDB        = errors.New("[Failed to insert row to the DB]")
 )
+
+// FailedDBConnection will take the error from the DB, pre-pend a simple message with the complex message.
+func FailedDBConnection(err error) error {
+	return formatErr(ErrFailedToOpenDBConnection, err)
+}
+
+// FailedToInsertToDB will take the error from the DB, pre-pend a simple message with the complex message.
+func FailedToInsertToDB(err error) error {
+	return formatErr(ErrFailedToInsertToDB, err)
+}
+
+func formatErr(simpleErr, err error) error {
+	message := fmt.Sprintf(simpleErr.Error() + ": " + err.Error())
+
+	return errors.New(message)
+}
