e796a20ae891303e1c52d944a83ce5bd85137cd1
ccdle12 <chris.coverdale24@gmail.com>
2018-09-08 14:20:36 +0100

    crawler/injector: updating injector with retrieve env vars

--- go-crawler/injector/injector.go
+++ go-crawler/injector/injector.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ccdle12/Blocksage/go-crawler/models"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -41,10 +42,48 @@ func BTCDomain() string {
 
 // BTCUsername will retrieve the environment variable for the password of the BTC Mainnet Node.
 func BTCUsername() string {
-	return os.Getenv("USERNAME")
+	return os.Getenv("BTC_USERNAME")
 }
 
 // BTCPassword will retrieve the environment variable for the username of the BTC Mainnet Node.
 func BTCPassword() string {
-	return os.Getenv("PASSWORD")
+	return os.Getenv("BTC_PASSWORD")
+}
+
+// PostgresDBName will retrieve the environment variable for the DB Name of the Postgres DB.
+func PostgresDBName() string {
+	return os.Getenv("POSTGRES_DB_NAME")
+}
+
+// PostgresUserName will retrieve the environment variable for the Username of the Postgres DB.
+func PostgresUserName() string {
+	return os.Getenv("POSTGRES_USER")
+}
+
+// PostgresPassword will retrieve the environment variable for the Password of the Postgres DB.
+func PostgresPassword() string {
+	return os.Getenv("POSTGRES_PASSWORD")
+}
+
+// PostgresDomain will retrieve the environment variable for the Postgres Domain of the Postgres DB.
+func PostgresDomain() string {
+	domain := splitDBDomain()[0]
+
+	return domain
+}
+
+// PostgresPort will retrieve the environment variable for the Postgres Port of the Postgres DB.
+func PostgresPort() string {
+	host := splitDBDomain()[1]
+
+	return host
+}
+
+// splitDBDomain will split the DB into to items into a []string{domain, host}.
+// The reasoning behind splittin the environment variable is when deployed using k8's,
+// we are going to use DNS to link each service. The DNS route the end points as <host:domain>.
+func splitDBDomain() []string {
+	address := os.Getenv("POSTGRES_DB_DOMAIN")
+
+	return strings.Split(address, ":")
 }
