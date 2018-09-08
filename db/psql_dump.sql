00de754752916da2495312b5e5288eb14cd6bbcb
ccdle12 <chris.coverdale24@gmail.com>
2018-09-08 14:17:48 +0100

    crawler/db: adding testblocks table

--- db/psql_dump.sql
+++ db/psql_dump.sql
@@ -13,7 +13,7 @@ CREATE TABLE "blocks" (
     version bigint NOT NULL,
     versionHex text NOT NULL,
     merkleroot text UNIQUE NOT NULL,
-    tx text[] NOT NULL,
+    foreignkeytx text NOT NULL,
     time bigint NOT NULL,
     mediantime bigint NOT NULL,
     nonce bigint NOT NULL,
@@ -22,4 +22,28 @@ CREATE TABLE "blocks" (
     chainwork text NOT NULL,
     nextblockhash text,
     PRIMARY KEY("height")
+    -- TODO(ccdle12): Add foreign key for tx here as ('foreignkeytx')
+);
+
+DROP TABLE IF EXISTS "testblocks";
+
+CREATE TABLE "testblocks" (
+    hash text UNIQUE NOT NULL,
+    strippedsize bigint NOT NULL,
+    size bigint NOT NULL,
+    weight bigint NOT NULL,
+    height bigint NOT NULL,
+    version bigint NOT NULL,
+    versionHex text NOT NULL,
+    merkleroot text UNIQUE NOT NULL,
+    foreignkeytx text NOT NULL,
+    time bigint NOT NULL,
+    mediantime bigint NOT NULL,
+    nonce bigint NOT NULL,
+    bits text NOT NULL,
+    difficulty double precision NOT NULL,
+    chainwork text NOT NULL,
+    nextblockhash text,
+    PRIMARY KEY("height")
+     -- TODO(ccdle12): Add foreign key for tx here as ('foreignkeytx')
 );
