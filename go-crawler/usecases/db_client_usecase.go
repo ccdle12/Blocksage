abd14fb5cb6cdd3816a525b320b8b5bf2dca33a5
ccdle12 <chris.coverdale24@gmail.com>
2018-09-08 14:22:50 +0100

    crawler/usecases: updating db client with InsertBlock

--- go-crawler/usecases/db_client_usecase.go
+++ go-crawler/usecases/db_client_usecase.go
@@ -1,7 +1,12 @@
 package usecases
 
+import (
+	"github.com/ccdle12/Blocksage/go-crawler/models"
+)
+
 // DBClient is the usecase/implementation interface for all DB usecases.
 type DBClient interface {
 	OpenConnection() error
 	CloseConnection() error
+	InsertBlock(b *models.Block) error
 }
