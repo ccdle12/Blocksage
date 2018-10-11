55ed04f26a0895b439923fc60e6b54368d99ca0b
ccdle12 <chris.coverdale24@gmail.com>
2018-10-11 21:40:55 +0100

    crawler/main.go: adding crawl logic

--- crawler/main.go
+++ crawler/main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"github.com/ccdle12/Blocksage/crawler/db-client"
+	"github.com/ccdle12/Blocksage/crawler/indexer"
+	"github.com/ccdle12/Blocksage/crawler/injector"
+	"github.com/ccdle12/Blocksage/crawler/node-client"
+	"log"
+)
+
 func main() {
+	// Create db client controller.
+	var db dbclient.Controller
+	db, err := dbclient.New(
+		dbclient.DBPort(injector.PostgresPort()),
+		dbclient.DBName(injector.PostgresDBName()),
+		dbclient.DBUser(injector.PostgresUserName()),
+		dbclient.DBHost(injector.PostgresDomain()),
+		dbclient.DBPassword(injector.PostgresPassword()),
+		dbclient.PostgresClient())
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	// Create connection to the DB.
+	if err = db.Connect(); err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer db.Close()
+
+	// Create a node Controller to communicate with a Blockchain Node.
+	var node nodeclient.Controller
+	node = nodeclient.New(
+		injector.DefaultHTTPClient(),
+		injector.BTCDomain(),
+		injector.BTCUsername(),
+		injector.BTCPassword())
+
+	// Create the indexer.
+	indexer := indexer.New(node, db)
+	// TODO (ccdle12): "Hard Code" Genesis Block in the DB.
+	if err = indexer.Crawl(injector.Block1Hash); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
